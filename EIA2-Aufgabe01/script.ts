2abdab16d56c84abc530e95b9caf309f2cbd0851
Marc S <siegfried.marc01@gmail.com>
2022-03-24 12:56:41 +0100

    iuhiou

--- EIA2-Aufgabe01/script.ts
+++ EIA2-Aufgabe01/script.ts
@@ -1,1 +1,34 @@
 console.log("Expecto Patronum");
+
+let subjects : string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let predicates : string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let objects : string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+console.log(subjects, predicates, objects);
+
+for (let i: number = subjects.length; i > 0; i--) {
+    //console.log(i);
+    
+    let sentence: string = getVerse(subjects, predicates, objects);
+    console.log(sentence);
+}
+
+function getVerse(_subjects: string[], _predicates : string[], _objects: string[]) {
+
+    let sub : number = Math.floor(Math.random() * subjects.length);
+    let pre : number = Math.floor(Math.random() * subjects.length);
+    let obj : number = Math.floor(Math.random() * subjects.length);
+
+    let Vers : string = (" ");
+
+    let a: string = _subjects[sub] + Vers + _predicates[pre] + Vers + _objects[obj];
+
+    _subjects.splice(sub, 1);
+    _predicates.splice(pre, 1);
+    _objects.splice(obj, 1);
+
+//console.log(sub);
+
+    return a;
+
+}
